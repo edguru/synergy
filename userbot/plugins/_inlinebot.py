05f3c194157805114b9688419f91091d9b079ac4
edguru <72596185+edguru@users.noreply.github.com>
2020-11-15 19:50:57 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -20,7 +20,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
             result = builder.article(
 
-                "© @synergyOT,
+                "© @synergyOT",
                 ALIVE_PIC
 
                 text="{}\n hi see my master's devil workshop \nCurrently Loaded Plugins: {}".format(
