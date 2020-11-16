74dd99c0a0c5184e6cde746a026880163a0ecef5
edguru <72596185+edguru@users.noreply.github.com>
2020-11-16 06:58:21 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -56,7 +56,7 @@ async def cmd_list(event):
         else:
             help_string = f"""Userbot Helper.. Provided by ✨{DEFAULTUSER}✨ \n
 `Userbot Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
-         await borg.send_file(event.chat_id,ALIVE_PIC,caption=help_string
+         await borg.send_file(event.chat_id,ALIVE_PIC,caption="😈😈 Welcome to the devil's workshop"
 
                                     
 
