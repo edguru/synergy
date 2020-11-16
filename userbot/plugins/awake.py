fe6cbb1a19c6f2964fe662af4cfbb3dcf753a42f
edguru <72596185+edguru@users.noreply.github.com>
2020-11-16 12:14:50 +0530

    Update awake.py

--- userbot/plugins/awake.py
+++ userbot/plugins/awake.py
@@ -26,7 +26,29 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 
 ALIVE_MESSAGE = Config.ALIVE_MSG
 if ALIVE_MESSAGE is None:
-   ALIVE_MESSAGE = "**🔱SYNERGY IS Awake🔱 \n\n\n**"
+
+
+
+
+
+
+ALIVE_MESSAGE = "**THE DEVIL HAS BEEN AWAKEN BY**"
+
+ALIVE_MESSAGE += f"**M̴y̴ ̴̴̴̴̴̴̴̴̴̴̴̴̴̴̴̴MA⃨s̤̈T̰̃E̫尺**            : {DEFAULTUSER}\n"
+
+ALIVE_MESSAGE += "ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀꜱɪᴏɴ        :  15.0.0 \n"
+
+ALIVE_MESSAGE += "MAGIC CHANNEL          : [ᴊᴏɪɴ](https://t.me/SynergysupportOfficial)\n"
+
+ALIVE_MESSAGE += "DARKEST GROUP        : [ᴊᴏɪɴ](https://t.me/synergyOT)\n"
+
+ALIVE_MESSAGE += "ʟɪᴄᴇɴꜱᴇ                 : [ᴍɪᴛ ʟɪᴄᴇɴꜱᴇ](https://github.com/edguru/synergy/blob/master/LICENSE)\n"
+
+ALIVE_MESSAGE += "ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ            : [gym2105](https://t.me/Gym2105)\n"
+
+ALIVE_MESSAGE += "Wooshhh💨💨💨 /n Looks like my master {DEFAULTUSER} HAS SUMMONED ME. /n It's time for me to perform my duties . /n hey those who go against my master's will fear my eye of thousand years of death👁️. And those in the good book thanks for being with my master . You Are a true friend🤝"
+
+ALIVE_MESSAGE +=" [synergy](https://t.me/synergyOT)"
                 
             
 #@command(outgoing=True, pattern="^.awake$")
