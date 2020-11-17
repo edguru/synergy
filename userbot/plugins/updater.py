b79e0ea6469a171598e6599f492d86614586c395
edguru <72596185+edguru@users.noreply.github.com>
2020-11-17 18:44:21 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -61,11 +61,11 @@ async def upstream(ups):
         origin = repo.create_remote('upstream', off_repo)
         origin.fetch()
         force_update = True
-        repo.create_head('master', origin.refs.master)
-        repo.heads.master.set_tracking_branch(origin.refs.master)
-        repo.heads.master.checkout(True)
+        repo.create_head('test', origin.refs.test)
+        repo.heads.test.set_tracking_branch(origin.refs.test)
+        repo.heads.test.checkout(True)
     ac_br = repo.active_branch.name
-    if ac_br != 'master':
+    if ac_br != 'test':
         await ups.edit(
             f'**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). '
             'in that case, Updater is unable to identify '
