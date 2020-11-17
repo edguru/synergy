6a4656ac5527fb58312d518ffa349261f39eabae
edguru <72596185+edguru@users.noreply.github.com>
2020-11-18 01:15:04 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,7 +5,38 @@ from userbot import ALIVE_NAME
 from userbot.plugins.alimg import POTO
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "My Boss"
-PM_IMG = POTO
+IMAGE_PAC = [
+
+    "https://telegra.ph/file/b2c76ef1d0a772cba9443.jpg",
+
+    "https://telegra.ph/file/62f0d475ab6cfa7136d01.jpg",
+
+    "https://telegra.ph/file/bd32c699e7f3d45d159ca.jpg",
+
+    "https://telegra.ph/file/4b5eeae024918d1ef1e71.jpg",
+
+    "https://telegra.ph/file/dc254dfc4fabc3223bb36.jpg",
+
+    "https://telegra.ph/file/1ab92cc6f82851b0a7106.jpg",
+
+    "https://telegra.ph/file/e8437f5d755735e66dc88.jpg",
+
+    "https://telegra.ph/file/6015c38ff8ac04b0852e1.jpg",
+
+    "https://telegra.ph/file/79b85e6fe298fbd4b9aba.jpg",
+
+    "https://telegra.ph/file/3cfc5220a29d0cfda889b.jpg",
+
+    "https://telegra.ph/file/bfaad699de64bb30cde81.jpg",
+
+    "https://telegra.ph/file/e89d4e34f6a458766f51d.jpg",
+
+]
+
+
+
+POTTO = random.choice(IMAGE_PAC)
+PM_IMG = POTTO
 pm_caption = "**Synergy ɪꜱ ON IT'S TOES**\n"
 
 pm_caption += f"**M̴y̴ ̴̴̴̴̴̴̴̴̴̴̴̴̴̴̴̴MA⃨s̤̈T̰̃E̫尺**            : {DEFAULTUSER}\n"
